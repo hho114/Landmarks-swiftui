3cc81cde957af9017f76afdffa6c8f85720a8325
Alex Ho <huyho114@gmail.com>
2021-05-01 00:49:51 -0400

    finish list and binding state

--- Landmarks/Views/Helpers/CircleImage.swift
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("kshow_logo")
-            .resizable().aspectRatio(contentMode: .fit).clipShape(Circle()).frame(width: 200,height: 200) .overlay(Circle().stroke(Color.white, lineWidth: 4)).shadow(radius: 7)
+        image.clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 4)).shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Alex Ho on 4/30/21.
+//
+
+import Foundation
